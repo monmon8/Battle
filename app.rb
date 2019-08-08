6440a788e5909fa9f1f3d2846e155cdffdd7f38f
monmon8 <student@Makerss-MacBook-Pro.local>
2019-08-08 14:35:09 +0100

    redirect/post

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   # get '/' do
   #   'Testing infrastructure working!'
@@ -11,9 +12,13 @@ class Battle < Sinatra::Base
   end 
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play 
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end 
-
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
+    erb :play
+  end
 end
